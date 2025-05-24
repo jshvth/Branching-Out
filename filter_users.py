9f7ce60cad8397647ce9cbbd55f96cc89265c852
Joshua Väth <joshua.vaeth@yahoo.de>
2025-05-24 11:58:07 +0200

    Test with different ages

--- filter_users.py
+++ filter_users.py
@@ -25,7 +25,7 @@ def load_users_from_json(filepath='users.json'):
 if __name__ == "__main__":
     users = load_users_from_json()
 
-    filtered_users = filter_by_age(users, min_age=28, max_age=30)
+    filtered_users = filter_by_age(users, min_age=18, max_age=30)
     print("Gefilterte User (25-30 Jahre):")
     for user in filtered_users:
         print(user)
